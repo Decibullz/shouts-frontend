591b74fe49c4709d7bca88553c7a550f209df125
Cody Snell <codyjsnell@yahoo.com>
2021-01-14 23:25:38 -0600

    installs axios, makes api call to get shouts

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+
+import Shout from "../components/Shout";
+
 export class home extends Component {
+  state = {
+    shouts: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/shouts")
+      .then((res) => {
+        this.setState({
+          shouts: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
   render() {
+    let recentShoutsMarkup = this.state.shouts ? (
+      this.state.shouts.map((shouts, idx) => <Shout key={idx} shout={shouts} />)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
-      <div>
-        <h1>Home page</h1>
-      </div>
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+          {recentShoutsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>profile,,,</p>
+        </Grid>
+      </Grid>
     );
   }
 }
