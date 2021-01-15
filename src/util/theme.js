f0a62825c035e860a4bcac31b9e0445c25ef3afd
Cody Snell <codyjsnell@yahoo.com>
2021-01-15 14:55:15 -0600

    creates theme file to clean up app.js

---
+++ src/util/theme.js
@@ -0,0 +1,19 @@
+export default {
+  palette: {
+    primary: {
+      light: "#e35183",
+      main: "#ad1457",
+      dark: "#78002e",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff5c8d",
+      main: "#d81b60",
+      dark: "#a00037",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+};
