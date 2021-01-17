8d862be10859944f05c7359d50e7dca0c02962f6
Cody Snell <codyjsnell@yahoo.com>
2021-01-17 02:54:16 -0600

    exports function to get user data

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -118,6 +118,24 @@ export const deleteShout = (shoutId) => (dispatch) => {
     .catch((err) => console.log(err));
 };
 
+export const getUserData = (userHandle) => (dispatch) => {
+  dispatch({ type: LOADING_DATA });
+  axios
+    .get(`/user/${userHandle}`)
+    .then((res) => {
+      dispatch({
+        type: SET_SHOUTS,
+        payload: res.data.shouts,
+      });
+    })
+    .catch(() => {
+      dispatch({
+        type: SET_SHOUTS,
+        payload: null,
+      });
+    });
+};
+
 export const clearErrors = () => (dispatch) => {
   dispatch({ type: CLEAR_ERRORS });
 };
