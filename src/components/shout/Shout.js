f1051a6de454b633e3eed87c299818ed0b22341d
Cody Snell <codyjsnell@yahoo.com>
2021-01-16 23:53:52 -0600

    File structure

---
+++ src/components/shout/Shout.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import MyButton from "../../util/MyButton";
+import ChatIcon from "@material-ui/icons/Chat";
+import DeleteShout from "./DeleteShout";
+import ShoutDialog from "./ShoutDialog";
+import LikeButton from "./LikeButton";
+const styles = {
+  card: {
+    position: "relative",
+    display: "flex",
+    marginBottom: 20,
+  },
+  image: {
+    minWidth: 200,
+  },
+  content: {
+    padding: 25,
+    objectFit: "cover",
+  },
+};
+
+export class Shout extends Component {
+  render() {
+    dayjs.extend(relativeTime);
+    const {
+      classes,
+      shout: {
+        body,
+        createdAt,
+        userImage,
+        userHandle,
+        shoutId,
+        likeCount,
+        commentCount,
+      },
+      user: {
+        authenticated,
+        credentials: { handle },
+      },
+    } = this.props;
+
+    const deleteButton =
+      authenticated && userHandle === handle ? (
+        <DeleteShout shoutId={shoutId} />
+      ) : null;
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            variants="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          {deleteButton}
+          <Typography variant="body2" color="textSecondary">
+            {dayjs(createdAt).fromNow()}
+          </Typography>
+          <Typography variant="body1">{body}</Typography>
+          <LikeButton shoutId={shoutId} />
+          <span>{likeCount} Likes</span>
+          <MyButton tip="Comments">
+            <ChatIcon color="primary" />
+          </MyButton>
+          <span>{commentCount} Comments</span>
+          <ShoutDialog shoutId={shoutId} userHandle={userHandle} />
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+Shout.propTypes = {
+  user: PropTypes.object.isRequired,
+  shout: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps)(withStyles(styles)(Shout));
