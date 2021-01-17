e83107bcbe43c1a3cc9d72e29ee020c6c97864a2
Cody Snell <codyjsnell@yahoo.com>
2021-01-16 21:17:10 -0600

    creates styled and fully functional component

---
+++ src/components/ShoutDialog.js
@@ -0,0 +1,154 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import MyButton from "../util/MyButton";
+import dayjs from "dayjs";
+import { Link } from "react-router-dom";
+// Material UI
+import withStyles from "@material-ui/core/styles/withStyles";
+import Dialog from "@material-ui/core/Dialog";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import DialogContent from "@material-ui/core/DialogContent";
+// Icons
+import CloseIcon from "@material-ui/icons/Close";
+import UnfoldMore from "@material-ui/icons/UnfoldMore";
+// REDUX
+import { connect } from "react-redux";
+import { getShout } from "../redux/actions/dataActions";
+
+const styles = {
+  invisibleSeparator: {
+    border: "none",
+    margin: 4,
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
+    objectFit: "center",
+    borderRadius: "50%",
+  },
+  dialogContent: {
+    padding: 20,
+  },
+  closeButton: {
+    position: "absolute",
+    left: "90%",
+  },
+  expandbutton: {
+    position: "absolute",
+    left: "90%",
+  },
+  spinnerDiv: {
+    textAlign: "center",
+    margin: "50 0",
+  },
+};
+
+class ShoutDialog extends Component {
+  state = {
+    open: false,
+  };
+  handleOpen = () => {
+    this.setState({
+      open: true,
+    });
+    this.props.getShout(this.props.shoutId);
+  };
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+  render() {
+    const {
+      classes,
+      shout: {
+        shoutId,
+        body,
+        createdAt,
+        likeCount,
+        commentCount,
+        userImage,
+        userHandle,
+      },
+      UI: { loading },
+    } = this.props;
+    const dialogMarkup = loading ? (
+      <div className={classes.spinnerDiv}>
+        <CircularProgress size={200} thickness={2} />
+      </div>
+    ) : (
+      <Grid container spacing={4}>
+        <Grid item sm={5}>
+          <img src={userImage} alt="Profile" className={classes.profileImage} />
+        </Grid>
+        <Grid item sm={7}>
+          <Typography
+            component={Link}
+            color="primary"
+            variant="h5"
+            to={`/users/${userHandle}`}
+          >
+            @{userHandle}
+          </Typography>
+          <hr className={classes.invisibleSeparator} />
+          <Typography variant="body2" color="textSecondary">
+            {dayjs(createdAt).format("h:mm a, MMM DD YYYY")}
+          </Typography>
+          <hr className={classes.invisibleSeparator} />
+          <Typography variant="body1">{body}</Typography>
+        </Grid>
+      </Grid>
+    );
+    return (
+      <>
+        <MyButton
+          onClick={this.handleOpen}
+          tip="Expand Shout"
+          tipClassName={classes.expandbutton}
+        >
+          <UnfoldMore color="primary" />
+        </MyButton>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <MyButton
+            tip="Exit"
+            onClick={this.handleClose}
+            tipClassName={classes.closeButton}
+          >
+            <CloseIcon />
+          </MyButton>
+          <DialogContent className={classes.dialogContent}>
+            {dialogMarkup}
+          </DialogContent>
+        </Dialog>
+      </>
+    );
+  }
+}
+ShoutDialog.propTypes = {
+  getShout: PropTypes.func.isRequired,
+  shoutId: PropTypes.string.isRequired,
+  userHandle: PropTypes.string.isRequired,
+  shout: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  shout: state.data.shout,
+  UI: state.UI,
+});
+
+const mapActionsToProps = {
+  getShout,
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(ShoutDialog));
